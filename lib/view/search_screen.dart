7722a5122282c41a456f6338aae9340b2e8cefd7
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-07-23 00:11:35 +0900

    2023-07-23 Polygons 선 구현 완료

--- lib/view/search_screen.dart
+++ lib/view/search_screen.dart
@@ -17,51 +17,49 @@ class MapSampleState extends State<SearchScreen> {
 
   final TextEditingController _searchController = TextEditingController();
 
+  Set<Marker> _markers = Set<Marker>();
+  Set<Polygon> _polygons = Set<Polygon>();
+
+  List<LatLng> polygonLatLngs = <LatLng>[];
+
+  int _polygonIdCounter = 1;
+
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static const Marker _kGooglePlexMarker = Marker(
-    markerId: MarkerId('_kGooglePlex'),
-    infoWindow: InfoWindow(title: 'Google Plex'),
-    icon: BitmapDescriptor.defaultMarker,
-    position: LatLng(37.42796133580664, -122.085749655962),
-  );
 
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  static final Marker _kLateMarker = Marker(
-    markerId: const MarkerId('_kLateMarker'),
-    infoWindow: const InfoWindow(title: 'Lake'),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-    position: const LatLng(37.43296265331129, -122.08832357078792),
-  );
-
-  static const Polyline _kPolyLine = Polyline(
-    polylineId: PolylineId('_kPolyLine'),
-    points: [
+  @override
+  void initState() {
+    super.initState();
+    
+    _setMarker (
       LatLng(37.42796133580664, -122.085749655962),
-      LatLng(37.43296265331129, -122.08832357078792),
-    ],
-    width: 5,
-  );
+    );
+  }
 
-  static const Polygon _kPolyGon = Polygon(
-    polygonId: PolygonId('_kPolygon'),
-    points: [
-      LatLng(37.42796133580664, -122.085749655962),
-      LatLng(37.43296265331129, -122.08832357078792),
-      LatLng(37.418, -122.092),
-      LatLng(37.435, -122.092),
-    ],
-    strokeWidth: 5,
-    fillColor: Colors.transparent,
-  );
+  void _setMarker (LatLng point) {
+    setState(() {
+      _markers.add (
+        Marker(markerId: MarkerId('marker'), position: point),
+      );
+    });
+  }
+
+  void _setPolygon() {
+    final String polygonIdVal = 'polygon_$_polygonIdCounter';
+    _polygonIdCounter++;
+
+    _polygons.add (
+      Polygon (
+          polygonId: PolygonId(polygonIdVal),
+          points: polygonLatLngs,
+        strokeWidth: 2,
+        fillColor: Colors.transparent,
+      )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,25 +89,22 @@ class MapSampleState extends State<SearchScreen> {
           Expanded(
             child: GoogleMap(
               mapType: MapType.normal,
-              markers: {_kGooglePlexMarker, _kLateMarker},
-              polylines: {
-                _kPolyLine,
-              },
-              polygons: {
-                _kPolyGon,
-              },
+              markers: _markers,
+              polygons: _polygons,
+
               initialCameraPosition: _kGooglePlex,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
+              onTap: (point) {
+                setState(() {
+                  polygonLatLngs.add(point);
+                  _setPolygon();
+                });
+              },
             ),
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
       ),
     );
   }
@@ -119,13 +114,14 @@ class MapSampleState extends State<SearchScreen> {
     final double lng = place['geometry']['location']['lng'];
 
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(
-      CameraPosition(target: LatLng(lat, lng), zoom: 12),
-    ));
-  }
+    await controller.animateCamera (
+        CameraUpdate.newCameraPosition (
+          CameraPosition(target: LatLng(lat, lng), zoom: 12),
+        )
+    );
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    _setMarker (
+      LatLng(lat, lng)
+    );
   }
 }

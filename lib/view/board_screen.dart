0000943b0b8a4d27073c714f0e89fb46889b7d96
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-08-23 23:40:45 +0900

    2023-08-20 Dialog API 영화 데이터 조회

--- lib/view/board_screen.dart
+++ lib/view/board_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_app_project/api/net/fetch.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +17,9 @@ class BoardScreen extends StatefulWidget {
 }
 
 class _BoardScreenState extends State<BoardScreen> {
+
+  List<dynamic> movieList = [];
+
   final FirebaseAuth _nicknameAuth = FirebaseAuth.instance;
   final FirebaseDatabase _nickNameRef = FirebaseDatabase.instance;
   final _movieKey = GlobalKey<FormState>();
@@ -41,6 +45,18 @@ class _BoardScreenState extends State<BoardScreen> {
     super.initState();
 
     _loadNickName();
+
+    ( () async {
+      var json = await fetch('/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json', {
+        'targetDt': '20221017'
+      });
+
+      setState(() {
+        movieList = json['boxOfficeResult']['dailyBoxOfficeList'];
+      });
+    })();
+    
+
   }
 
   @override
@@ -54,149 +70,115 @@ class _BoardScreenState extends State<BoardScreen> {
             barrierDismissible: true,
             builder: (BuildContext context) {
 
-              return Dialog (
-                shape: RoundedRectangleBorder (
-                  borderRadius: BorderRadius.circular(10.0)
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: double.infinity,
-                        height: 560,
-                        padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
-                        child: Column (
-                          children: [
-                            const Text (
-                              "좋아하는 영화를 소개!",
-                                style: TextStyle (
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                            ),
-
-                            const SizedBox (
-                              height: 30.0,
-                            ),
-
-                            Row (
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-
-                              children: [
-
-                                const Text (
-                                  '이름: ',
-                                  style: TextStyle (
-                                    fontSize: 18,
-                                    color: Colors.black,
-                                  ),
-                                ),
-
-                                const SizedBox (
-                                  width: 4.0,
-                                ),
-
-                                Text (
-                                  _nickName,
-                                  style: const TextStyle (
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ],
-                            ),
-
-                            const SizedBox (
-                              height: 28.0,
-                            ),
-
-                            const Row (
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-
-                              children: [
-                                Text (
-                                  '영화를 추천하고 싶어요!!',
-                                  style: TextStyle (
-                                    fontSize: 22.0,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ],
-                            ),
-
-                            const SizedBox (
-                              height: 16.0,
-                            ),
-
-                            Row (
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    showDialog (
-                                        context: context,
-                                        barrierDismissible: true,
-
-                                        builder: (BuildContext context) {
-                                          return Dialog (
-                                            child: Stack (
-                                              children: [
-                                                Container (
-                                                  width: double.infinity,
-                                                  height: 560,
-                                                  padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
-                                                  child: Column (
-                                                    children: [
-                                                      Form (
-                                                        key: _movieKey,
-                                                        child: TextFormField (
-                                                          validator: (value) {
-
-                                                          },
-                                                          controller: _movieController,
-                                                          cursorRadius: const Radius.circular(40),
-                                                          style: const TextStyle (
-                                                            color: Colors.black,
-                                                          ),
-                                                          decoration: const InputDecoration (
-                                                            enabledBorder: UnderlineInputBorder (
-                                                              borderSide: BorderSide (color: Colors.black),
-                                                            ),
-                                                            hintText: '좋아하는 영화 이름을 검색해주세요.',
-                                                            hintStyle: TextStyle (
-                                                              color: Colors.grey,
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      ),
-                                                    ],
-                                                  ),
-                                                )
-                                              ],
-                                            ),
-                                          );
-                                        }
-                                    );
-                                  },
-
-                                  child: const Text (
-                                    '영화 입력하러 가기',
-                                    style: TextStyle (
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
-                                )
-                              ],
-                            )
-                          ],
+                child: Container(
+                  width: double.infinity,
+                  height: 560,
+                  padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "좋아하는 영화를 소개!",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '이름: ',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 4.0,
+                          ),
+                          Text(
+                            _nickName,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 28.0,
+                      ),
+                      const Text(
+                        '영화를 추천하고 싶어요!!',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            barrierDismissible: true,
+                            builder: (BuildContext context) {
+                              return Dialog(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Form(
+                                      key: _movieKey,
+                                      child: Padding (
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: TextFormField (
+                                          controller: _movieController,
+                                          decoration: const InputDecoration(
+                                            hintText: '영화 이름을 입력해주세요',
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10), // Add spacing between Form and ListView
+                                    Expanded (
+                                      child: Padding (
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: ListView.separated(
+                                          shrinkWrap: true,
+                                          itemBuilder: (context, index) {
+                                            return Text('${index + 1} : ${movieList[index]['movieNm']}');
+                                          },
+                                          separatorBuilder: (context, index) => const Divider(),
+                                          itemCount: movieList.length,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: const Text(
+                          '영화 입력하러 가기',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      )
                     ],
-                  )
+                  ),
+                ),
               );
             }
           );
